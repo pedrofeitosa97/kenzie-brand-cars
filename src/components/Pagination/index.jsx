971cf51ed518ef545d90bdcfd4ad35dee607b87b
Paulo Vitor <patoassado@gmail.com>
2023-04-15 20:23:21 -0300

    fix:pagination style

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -3,25 +3,50 @@ import { HomeContext } from "../../Providers/homeContext";
 import { PaginationDivStyle } from "./style";
 
 export default function Pagination() {
-  const { handlePageClick, currentPage, totalPages } = useContext(HomeContext);
+  const { currentPage, totalPages } = useContext(HomeContext);
+  // const [buttonClass1, setButtonClass1] = useState()
+  // const [buttonClass2, setButtonClass2] = useState()
+
+  // function handleButtonClass(){
+  //   currentPage === totalPages
+  //   setButtonClass2()
+  // }
 
   return (
     <PaginationDivStyle>
-      <button
-        onClick={(event) => handlePageClick(event, currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        Anterior
-      </button>
+      {currentPage !== 1 ? <ButtonBackward /> : <></>}
       <span>
         {currentPage} de {totalPages}
       </span>
-      <button
-        onClick={(event) => handlePageClick(event, currentPage + 1)}
-        disabled={currentPage === totalPages}
-      >
-        Seguinte
-      </button>
+      {currentPage !== totalPages ? <ButtonForward /> : <></>}
     </PaginationDivStyle>
+  );
+}
+
+function ButtonBackward() {
+  const { handlePageClick, currentPage } = useContext(HomeContext);
+  return (
+    <button
+      onClick={(event) => {
+        handlePageClick(event, currentPage - 1);
+      }}
+      disabled={currentPage === 1}
+    >
+      Anterior
+    </button>
+  );
+}
+
+function ButtonForward() {
+  const { handlePageClick, currentPage, totalPages } = useContext(HomeContext);
+  return (
+    <button
+      onClick={(event) => {
+        handlePageClick(event, currentPage + 1);
+      }}
+      disabled={currentPage === totalPages}
+    >
+      Seguinte
+    </button>
   );
 }
--- src/components/Pagination/style.js
+++ src/components/Pagination/style.js
@@ -5,4 +5,19 @@ export const PaginationDivStyle = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  span {
+    margin: 0px 23px 0px 23px;
+    font-size: 24px;
+    font-weight: 600;
+  }
+
+  button {
+    background-color: white;
+    border: none;
+    color: var(--collor-brand-2);
+    cursor: pointer;
+    font-size: 24px;
+    font-weight: 600;
+  }
 `;
