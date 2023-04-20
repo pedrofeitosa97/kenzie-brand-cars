a43b06074fc18dd2ec29cfec0cf9984d7d0ad5e1
Jhonatas <jhonatasoliveira021@gmail.com>
2023-04-20 16:41:27 -0300

    fix: refactor develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { DetailAnnouncementPage } from './pages/detail_announce';
 import Login from './pages/login_page';
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 function App() {
   return (
-    <Login/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='' Component={Login}/>
+      <Route path='register' Component={Registe}/>
+    </Routes>
+    </BrowserRouter>
   )
-  // return (
-  //   <DetailAnnouncementPage/>
-  // )
 }
 
 export default App;
